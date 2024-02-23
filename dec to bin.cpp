3b33c942a7235e713e895223219f45c7c6c1255d
Sadiq <53291095+sadiqshaik123@users.noreply.github.com>
2024-02-23 09:17:06 +0530

    Update dec to bin.cpp

--- dec to bin.cpp
+++ dec to bin.cpp
@@ -1,17 +1,23 @@
 #include <iostream>
 #include <bitset>
-
 using namespace std;
 
 int main() {
+    int num;
+    cin >> num;
+    
+    bitset<4> binaryNumber(num);
+    cout << "Binary representation: " << binaryNumber << endl;
 
-    int decimalNumber;
-    cout << "Enter a decimal number: ";
-    cin >> decimalNumber;
+    int ans[2];
+    for (int i = 0; i < 4; i += 2) {
+        ans[i / 2] = binaryNumber[i] + binaryNumber[i + 1];
+    }
 
-    bitset<4> binaryNumber(decimalNumber);
-
-    cout << "Binary equivalent: " << binaryNumber << endl;
+    cout << "Sum of pairs of bits: ";
+    for (int i = 0; i < 2; i++) {
+        cout << ans[i] << " ";
+    }
 
     return 0;
 }
